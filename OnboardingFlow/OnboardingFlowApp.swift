ef8e08a85306d055884e92f2342ae39d47359362
kotathefriends <5070kouta@gmail.com>
2023-12-02 14:26:39 +0900

    Initial Commit

---
+++ OnboardingFlow/OnboardingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowApp.swift
+//  OnboardingFlow
+//
+//  Created by KOTA TAKAHASHI on 2023/12/02.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
